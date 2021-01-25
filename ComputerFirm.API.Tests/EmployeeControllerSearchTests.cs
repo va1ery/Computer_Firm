726459d744544e873e460162802da367065384b5
Valery Gurov <va1ery@ya.ru>
2021-01-25 11:39:15 +0300

    first commit

---
+++ ComputerFirm.API.Tests/EmployeeControllerSearchTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using ComputerFirm.API.Controllers;
+
+namespace ComputerFirm.API.Tests
+{
+    public class EmployeeControllerSearchTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var controller = new EmployeeController();
+        }
+    }
+}
